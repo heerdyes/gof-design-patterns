ee9aaf397adc5f628467e6e99b4f76d34d437d3c
Heerdyes Mahapatro <heerdyes.mahapatro@gmail.com>
2022-01-07 23:44:57 +0530

    set up latex doc generation pipeline

--- singleton/singletondemo.py
+++ singleton/singletondemo.py
@@ -18,9 +18,31 @@ class Turtlebot:
         return 'turtlebot: %d, %d, %d'%(self.x,self.y,self.a)
 
 
+class Turtleswarm:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            print('creating instance for the first time')
+            cls._instance = super(Turtleswarm, cls).__new__(cls)
+            cls._instance.tbots=[]
+        return cls._instance
+        
+    def __str__(self):
+        s=[]
+        for tbot in self.tbots:
+            s.append(str(tbot))
+        return '\n'.join(s)
+
+
 #t0=Turtlebot(3,4,0) # this is rendered impossible
 t0=Turtlebot.instance(3,4,0)
 print(t0)
 t1=Turtlebot.instance(6,8,90) # parameters have no effect, simply returns the existing _instance
 print(t1)
+# another way
+ts0=Turtleswarm()
+ts0.tbots.append(t1)
+print(ts0)
+ts1=Turtleswarm()
 
